4a7b3bf532e72dbce239d78237155903fa430d21
Wizer27 <svd2yar@gmail.com>
2026-01-16 13:42:14 +0300

    core writen

--- backend/database/core.py
+++ backend/database/core.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 
 async_engine = create_async_engine(
-    f"postgresql+asyncpg://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@localhost:5432/photo_saver_def",
+    f"postgresql+asyncpg://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@localhost:5432/character_ai_start",
     pool_size=20,           
     max_overflow=50,       
     pool_recycle=3600,      
@@ -39,3 +39,63 @@ async def create_table():
 async def drop_table():
     async with async_engine.begin() as conn:
         await conn.run_sync(metadata_obj.drop_all) 
+        
+
+async def get_all_data() -> List:
+    async with AsyncSession(async_engine) as conn:
+        try:
+            stmt = select(table)
+            res = await conn.execute(stmt)
+            return list(res.fetchall())
+        except exc.SQLAlchemyError:
+            raise exc.SQLAlchemyError("Error while executing")
+
+async def is_user_exists(username:str) -> bool:
+    async with AsyncSession(async_engine) as conn:
+        try:
+            stmt = select(table.c.username).where(table.c.username == cast(username,String))
+            res = await conn.execute(stmt)
+            data = res.scalar_one_or_none()
+            if data is not None:
+                return str(data) == username
+            return False
+        except exc.SQLAlchemyError:
+            raise exc.SQLAlchemyError("Error while executing")  
+
+
+async def create_user(user_id:str) -> bool:
+    async with AsyncSession(async_engine) as conn:
+        async with conn.begin():
+            try:
+                stmt = table.insert().values(
+                    username = cast(user_id,String),
+                    sub = False
+                )
+                await conn.execute(stmt)
+                return True
+            except exc.SQLAlchemyError:
+                raise exc.SQLAlchemyError("Error while executing")
+
+async def subscribe(username:str) -> bool:
+    if not await is_user_exists(username):
+        return False
+    async with AsyncSession(async_engine) as conn:
+        async with conn.begin():
+            try:
+                stmt = table.update().where(table.c.username == username).values(sub = True)
+                await conn.execute(stmt)
+                return True
+            except exc.SQLAlchemyError:
+                raise exc.SQLAlchemyError("Error while executing")
+
+async def is_user_subbed(username:str) -> bool:
+    async with AsyncSession(async_engine) as conn:
+        try:
+            stmt = select(table.c.sub).where(table.c.username == username)
+            res = await conn.execute(stmt)
+            data = res.scalar_one_or_none()
+            if data is not None:
+                return data
+            raise NameError("User not found") # поидеи никогда не произойдет 
+        except exc.SQLAlchemyError as conn:
+            raise exc.SQLAlchemyError("Error while executing")          
