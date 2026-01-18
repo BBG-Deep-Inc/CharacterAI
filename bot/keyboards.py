0e55d1feda0f0c554819c9eeeaf5eb80b8fcb4c2
Wizer27 <svd2yar@gmail.com>
2026-01-18 14:27:29 +0300

    started bot coding

---
+++ bot/keyboards.py
@@ -0,0 +1,6 @@
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardMarkup,InlineKeyboardButton
+
+
+main_keyborad = ReplyKeyboardMarkup(keyboard = [
+    [KeyboardButton(text = "Profile"),KeyboardButton(text = "Chat")]
+])
